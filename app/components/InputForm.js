345b2ec0c1040e1a29c1b88d5776781fba460e36
bmdonnell <bmdonnell@gmail.com>
2024-04-09 18:25:14 -0700

    Updated layoout to allow dropdown list of videos

--- app/components/InputForm.js
+++ app/components/InputForm.js
@@ -1,46 +1,91 @@
 // components/InputForm.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const InputForm = ({ onSubmit }) => {
-  const [baseUrl, setBaseUrl] = useState('');
-  const [inputList, setInputList] = useState(''); // Define inputList state
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState('');
+  const [inputList, setInputList] = useState([{ timestamp: '', text: '' }]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(baseUrl, inputList); // Pass both baseUrl and inputList to onSubmit
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+  const fetchVideos = async () => {
+    try {
+      const response = await axios.get('https://api.twitch.tv/helix/videos', {
+        headers: {
+          'Client-ID': process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID,
+          'Authorization': process.env.NEXT_PUBLIC_TWITCH_AUTH_TOKEN,
+        },
+        params: {
+          user_id: '44574567',
+          first: 50, // Number of videos to fetch
+        },
+      });
+      setVideos(response.data.data);
+    } catch (error) {
+      console.error('Error fetching videos:', error);
+    }
+  };
+
+  const handleInputChange = (index, event) => {
+    const { name, value } = event.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  const handleAddEntry = () => {
+    setInputList([...inputList, { timestamp: '', text: '' }]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const selectedVideoData = videos.find(
+      (video) => video.id === selectedVideo
+    );
+    onSubmit(selectedVideoData.url, inputList);
   };
 
   return (
     <div className='w-full'>
       <form
-        className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+        className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4  text-black'
         onSubmit={handleSubmit}>
-        <div className='mb-4'>
-          <label
-            className='block text-gray-700 text-sm font-bold mb-2'>
-           Video URL for Twitch or video id:
+        <div className='mb-4 w-96 overflow-hidden'>
+          <label>
+            Select Video:
+            <select
+              value={selectedVideo}
+              onChange={(event) => setSelectedVideo(event.target.value)}>
+              <option value=''>Select a Video</option>
+              {videos.map((video) => (
+                <option key={video.id} value={video.id}>{`${
+                  video.title
+                } - ${new Date(
+                  video.created_at
+                ).toLocaleDateString()}`}</option>
+              ))}
+            </select>
           </label>
-          <input
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-            id='bases'
-            type='text'
-            value={baseUrl} onChange={(e) => setBaseUrl(e.target.value)} 
-          />
         </div>
         <div className='mb-6'>
-          <label
-            className='block text-gray-700 text-sm font-bold mb-2'>
+          <label className='block text-gray-700 text-sm font-bold mb-2'>
             Paste the list of timestamps and text:
           </label>
           <textarea
             className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline h-96'
             id='list'
             type='text'
-            value={inputList} onChange={(e) => setInputList(e.target.value)}
+            onChange={(e) => setInputList(e.target.value)}
           />
         </div>
         <div className='flex items-center justify-between'>
-          <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button'>Generate Links</button>
+          <button
+            type='submit'
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button'>
+            Generate Links
+          </button>
         </div>
       </form>
     </div>
