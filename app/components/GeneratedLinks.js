f5fe984432fa7d81ea40e31c4e63b6fb8d77292e
bmdonnell <bmdonnell@gmail.com>
2024-04-14 19:07:56 -0700

    Cleaned up layout

--- app/components/GeneratedLinks.js
+++ app/components/GeneratedLinks.js
@@ -1,17 +1,17 @@
-// components/GeneratedLinks.js
 import React from 'react';
 
 const GeneratedLinks = ({ links }) => {
+  // Convert the list of links to a string
+  const linksString = links.map((link, index) => `${link.timestamp} - [${link.text}](${link.url})`).join('\n');
+
   return (
     <div className='w-full bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 overflow-auto'>
       <h2 className='text-black font-bold'>Generated Links:</h2>
-      <ul>
-        {links.map((link, index) => (
-          <li key={index} className="list-none text-gray-700">
-            {link.timestamp} - [{link.text}]({link.url})
-          </li>
-        ))}
-      </ul>
+      <textarea rows="14"
+        className="w-full border border-gray-300 rounded p-2 text-black"
+        value={linksString}
+        readOnly
+      />
     </div>
   );
 };
